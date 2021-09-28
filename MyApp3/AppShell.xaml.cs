4551d75cc5009ed5ed5bd6d059fd724b7f08b799
Jamie (JT) O'Roke <56612513+oroke24@users.noreply.github.com>
2021-09-28 14:10:44 -0700

    Add files via upload

---
+++ MyApp3/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MyApp3
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyApp3/Models/Money.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp3.Models
+{
+    public class Money
+    {
+       public String standing { get; set; }
+       public int amount { get; set; }
+       public string Image { get; set; }
+    }
+}
